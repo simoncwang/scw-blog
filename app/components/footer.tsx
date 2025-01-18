445fc87d77ba19b8ba3ee180e318dc8c24a9429a
Simon Wang <wang.c.simon@gmail.com>
2025-01-18 18:26:37 -0500

    first commit

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -18,8 +18,12 @@ function ArrowIcon() {
 export default function Footer() {
   return (
     <footer className="mb-16">
+      <hr className="mt-12 mb-0"></hr>
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
+        <li className="font-bold ">
+        created w/ vercel portfolio starter:
+        </li>
+        {/* <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
@@ -29,7 +33,7 @@ export default function Footer() {
             <ArrowIcon />
             <p className="ml-2 h-7">rss</p>
           </a>
-        </li>
+        </li> */}
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
@@ -38,7 +42,7 @@ export default function Footer() {
             href="https://github.com/vercel/next.js"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
+            <p className="ml-2 h-7">original github</p>
           </a>
         </li>
         <li>
@@ -49,12 +53,12 @@ export default function Footer() {
             href="https://vercel.com/templates/next.js/portfolio-starter-kit"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">template</p>
           </a>
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+        © {new Date().getFullYear()} Simon Wang
       </p>
     </footer>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,42 @@
 import { BlogPosts } from 'app/components/posts'
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Simon Wang
       </h1>
+      <div className="my-8">
+        <Image
+          src="/images/simon.jpg" // Path to the image (can be in public directory)
+          alt="Description of the image"
+          width={400} // Set the desired width
+          height={300} // Set the desired height
+          className="rounded-lg shadow-md" // Optional: Tailwind classes for styling
+        />
+      </div>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {
+        "Welcome to my little blog! Here is where I will share my thoughts on various topics I am interested in; including tech, art, artificial intelligence, and anything in between!"
+        }
+        
       </p>
+      <p className="mb-4">
+        {
+          "In my day-to-day, I am a Computer Science Master's student at the University of Maryland - College Park. For more info please check out my work in AI/ML and more here on my "
+        }
+        {
+          <a href="https://simoncwang.github.io/" target="_blank" className="text-blue-500 hover:underline">
+          technical portfolio site!
+        </a>
+        }
+      </p>
+      <p className="mb-4">
+        {
+          "An important note: my opinions and thoughts expressed here are not a representation of my university nor any of my colleagues!"
+        }
+        </p>
       <div className="my-8">
         <BlogPosts />
       </div>

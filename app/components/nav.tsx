1ac9df704fbc3d03744d953835dd5cc90341f26a
Simon Wang <wang.c.simon@gmail.com>
2025-01-18 23:40:54 -0500

    fixed link and add blank target

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -13,7 +13,7 @@ const navItems = {
   '/blog': {
     name: 'blog',
   },
-  'https://github.com/': {
+  'https://github.com/simoncwang': {
     name: 'github',
   },
   'https://simoncwang.github.io/': {
@@ -75,6 +75,7 @@ export function Navbar() {
               <Link
                 key={path}
                 href={path}
+                target="_blank"
                 className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
               >
                 {name}
